c229940d1fed2d0519935918fc528749ef2a3dcb
Aaron Kawer <aaronkawer@gmail.com>
2016-11-03 07:08:41 -0600

    Successful Authentication with Facebook and Firebase

--- DrHouse/LoginController.swift
+++ DrHouse/LoginController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 import Firebase
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 
 class LoginController: UIViewController {
@@ -19,9 +21,39 @@ class LoginController: UIViewController {
 
     }
     
+    // maybe delete this function to avoid unnecessary warnings
+    override func didReceiveMemoryWarning() {
+        super.viewDidLoad()
+        //Do any additional bla bla
+    }
        // MARK: - Functions
     
-
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("NEGROKO: Unable to authenticate with Facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("NEGROKO: User cancelled Facebook authentication")
+            } else {
+                print("NEGROKO: Successfully authenticated with Facebook \n")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+    }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("NEGROKO: Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("NEGROKO: Successfully authenticated with Firebase")
+            }
+            })
+    }
 
     /*
     // MARK: - Navigation
--- Podfile
+++ Podfile
@@ -10,6 +10,7 @@ target 'DrHouse' do
   pod 'Firebase/Auth’
   pod 'Firebase/Database’
   pod 'Firebase/Storage’
+  pod 'FBSDKLoginKit'
 
 
 end
