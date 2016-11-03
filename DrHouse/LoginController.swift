7046a88aa6bddf5cd484e650b341f30ad296c62f
Aaron Kawer <aaronkawer@gmail.com>
2016-11-03 05:22:51 -0600

    Basic sign in Layout + Firebase auth

---
+++ DrHouse/LoginController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginController.swift
+//  DrHouse
+//
+//  Created by Aaron Kawer on 11/2/16.
+//  Copyright © 2016 Aaron Kawer. All rights reserved.
+//
+
+import UIKit
+
+import Firebase
+
+
+class LoginController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+       // MARK: - Functions
+    
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pods/Target Support Files/Pods-DrHouse/Pods-DrHouse-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DrHouse : NSObject
+@end
+@implementation PodsDummy_Pods_DrHouse
+@end
